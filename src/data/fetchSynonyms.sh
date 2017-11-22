4ccbf228595c4e6376f4d94886f84b997edc4fdf
David Goldstein <dgoldstein01@gmail.com>
2017-11-22 10:34:35 -0500

    - created sh entrypoint

--- src/data/fetchSynonyms.sh
+++ src/data/fetchSynonyms.sh
@@ -1,1 +1,17 @@
-fetchSynonyms.sh
+# fetchSynonyms.sh
+
+# Created by David on 11/22/17
+# Entry point for fetching and saving synonyms of a given language
+# Usage : ./fetchingSynonyms ${language}
+# Currently supported languages : 'english'
+
+# check that language is supported
+
+language=$1
+if [ ! -d "${language}" ]; then
+  echo "No such language supported ${language}"
+  exit 1
+fi
+
+# run python script to write words to large json
+time python fetcher.py ${language}
